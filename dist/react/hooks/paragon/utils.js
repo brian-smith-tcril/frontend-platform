c8c5b6ea7ae2446060138debe251cf2e685c4029
Brian Smith <bsmith@axim.org>
2025-05-15 15:19:44 -0400

    chore: test some stuff

---
+++ dist/react/hooks/paragon/utils.js
@@ -0,0 +1,49 @@
+import { getConfig } from '../../../config';
+import { basename } from '../../../initialize';
+
+/**
+ * Iterates through each given `<link>` element and removes it from the DOM.
+ * @param {HTMLLinkElement[]} existingLinks
+ */
+export var removeExistingLinks = function removeExistingLinks(existingLinks) {
+  existingLinks.forEach(function (link) {
+    link.remove();
+  });
+};
+
+/**
+* Creates the fallback theme base URL.
+* @returns {string} The default theme base url.
+*/
+export var fallbackThemeBaseUrl = function fallbackThemeBaseUrl() {
+  var _window$location2;
+  // Always use window in development mode to ensure
+  // we have the correct port
+  if (getConfig().ENVIRONMENT === "development") {
+    var _window$location;
+    var config = getConfig();
+    debugger;
+    return (_window$location = window.location) === null || _window$location === void 0 ? void 0 : _window$location.origin;
+  }
+
+  // Otherwise, use BASE_URL from config if we have it
+  return getConfig().BASE_URL || ((_window$location2 = window.location) === null || _window$location2 === void 0 ? void 0 : _window$location2.origin);
+};
+
+/**
+* Creates the fallback URL for the given theme file.
+* @param {string} url The theme file path.
+* @returns {string} The default theme url.
+*/
+export var fallbackThemeUrl = function fallbackThemeUrl(url) {
+  var baseUrl = fallbackThemeBaseUrl();
+
+  // validates if the baseurl has the protocol to be interpreted correctly by the browser,
+  // if is not present add '//' to use Protocol-relative URL
+  var protocol = /^(https?:)?\/\//.test(baseUrl) ? '' : '//';
+  return "".concat(protocol).concat(baseUrl).concat(basename).concat(url);
+};
+export var isEmptyObject = function isEmptyObject(obj) {
+  return !obj || Object.keys(obj).length === 0;
+};
+//# sourceMappingURL=utils.js.map
--- src/react/hooks/paragon/utils.js
+++ src/react/hooks/paragon/utils.js
@@ -12,12 +12,29 @@ export const removeExistingLinks = (existingLinks) => {
 };
 
 /**
+* Creates the fallback theme base URL.
+* @returns {string} The default theme base url.
+*/
+export const fallbackThemeBaseUrl = () => {
+  // Always use window in development mode to ensure
+  // we have the correct port
+  if (getConfig().ENVIRONMENT === "development") {
+    const config = getConfig();
+    debugger;
+    return window.location?.origin;
+  }
+
+  // Otherwise, use BASE_URL from config if we have it
+  return getConfig().BASE_URL || window.location?.origin;
+};
+
+/**
 * Creates the fallback URL for the given theme file.
 * @param {string} url The theme file path.
 * @returns {string} The default theme url.
 */
 export const fallbackThemeUrl = (url) => {
-  const baseUrl = getConfig().BASE_URL || window.location?.origin;
+  const baseUrl = fallbackThemeBaseUrl();
 
   // validates if the baseurl has the protocol to be interpreted correctly by the browser,
   // if is not present add '//' to use Protocol-relative URL
